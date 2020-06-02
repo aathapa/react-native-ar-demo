de973447682fca7bf40b52ad6014a241b94f8903
Aayush Thapa <aayushthapa7@gmail.com>
2020-06-02 17:22:26 +0545

    ar setup

--- android/app/src/main/java/com/ar/MainApplication.java
+++ android/app/src/main/java/com/ar/MainApplication.java
@@ -4,12 +4,16 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.augment.reactplugin.AugmentReactPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+import com.viromedia.bridge.ReactViroPackage;
+import com.facebook.react.shell.MainReactPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new ReactViroPackage(ReactViroPackage.ViroPlatform.AR));
           return packages;
         }
 
